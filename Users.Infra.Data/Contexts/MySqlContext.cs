e162ef2066456301d6cc14663fa21fba435ba09f
AnaFMel <anaclara.freire.melo@gmail.com>
2026-06-18 21:34:39 -0300

    Alterando nome da variável de ambiente

--- Users.Infra.Data/Contexts/MySqlContext.cs
+++ Users.Infra.Data/Contexts/MySqlContext.cs
@@ -25,7 +25,7 @@ namespace Users.Infra.Data.Contexts
             if (!optionsBuilder.IsConfigured)
             {
                 var user = Environment.GetEnvironmentVariable("MARIADB_USER") ?? "root";
-                var password = Environment.GetEnvironmentVariable("MARIADB_PASSWORD") ?? "SenhaAdmin123!";
+                var password = Environment.GetEnvironmentVariable("MARIADB_ROOT_PASSWORD") ?? "SenhaAdmin123!";
                 var database = Environment.GetEnvironmentVariable("MARIADB_DATABASE") ?? "users_db";
                 var host = Environment.GetEnvironmentVariable("MARIADB_HOST") ?? "localhost";
                 var port = Environment.GetEnvironmentVariable("MARIADB_HOST") is null ? "3307" : "3306";
